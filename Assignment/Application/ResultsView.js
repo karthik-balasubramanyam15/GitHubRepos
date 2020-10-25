dccb19723c4eaee8dfba09b02002e3e7ab9266ef
Karthik <kbalasub@stevens.edu>
2020-10-25 01:21:43 -0500

    Replaced map with FlatList

--- Assignment/Application/ResultsView.js
+++ Assignment/Application/ResultsView.js
@@ -1,17 +1,45 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, FlatList } from 'react-native';
 import _ from 'lodash';
 
 class ResultsView extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+        this.display = this.display.bind(this);
+    }
 
     display(value) {
         return (
             <View style={{ flex: 1, margin: 10 }}>
-                <Text>{value.commitHash}</Text>
-                <Text>{value.author}</Text>
-                <Text>{value.commitMessage}</Text>
+                <View style={{ flexDirection: 'row', flex: 1, marginBottom: 5, marginTop: 5 }}>
+                    <View style={{ flex: 0.2 }}>
+                        <Text style={{ fontSize: 16 }}>Hash: </Text>
+                    </View>
+                    <View style={{ flex: 0.8 }}>
+                        <Text style={{ fontSize: 16, flexWrap: 'wrap' }}>{value.commitHash} </Text>
+                    </View>
+                </View>
+                <View style={{ flexDirection: 'row', flex: 1, marginBottom: 5, marginTop: 5 }}>
+                    <View style={{ flex: 0.2 }}>
+                        <Text style={{ fontSize: 16 }}>Author: </Text>
+                    </View>
+                    <View style={{ flex: 0.8 }}>
+                        <Text style={{ fontSize: 16, flexWrap: 'wrap' }}>{value.author}</Text>
+                    </View>
+                </View>
+                <View style={{ flexDirection: 'row', flex: 1, marginBottom: 5, marginTop: 5 }}>
+                    <View style={{ flex: 0.2 }}>
+                        <Text style={{ fontSize: 16 }}>Message: </Text>
+                    </View>
+                    <View style={{ flex: 0.8 }}>
+                        <Text style={{ fontSize: 16, flexWrap: 'wrap' }}>{value.commitMessage}</Text>
+                    </View>
+                </View>
             </View>
         )
     }
@@ -21,18 +49,18 @@ class ResultsView extends Component {
         let input = this.props && this.props.data && this.props.data.length > 0 ? this.props.data : null
         console.log("Input: ", input)
         return (
-            <ScrollView>
-                <View style={{ flex: 1, backgroundColor: '#ADD8E6' }}>
-                    {
-                        input && input.length > 0 ? input.map((value) => {
-                            return <View style={{ borderBottomColor: 'black', borderBottomWidth: 1 }}>
-                                {this.display(value)}
-                            </View>
-                        })
-                            : null
-                    }
-                </View>
-            </ScrollView>
+            <FlatList
+                style={{ flex: 1, backgroundColor: '#ADD8E6' }}
+                keyExtractor={(item) => item.commitHash}
+                data={input}
+                renderItem={({ item }) =>
+                    <View style={{ flex: 1, }}>
+                        {this.display(item)}
+                    </View>
+                }
+                ItemSeparatorComponent={() => <View style={{ borderBottomWidth: 1, borderBottomColor: 'black' }} />}
+            />
+
         );
     }
 }

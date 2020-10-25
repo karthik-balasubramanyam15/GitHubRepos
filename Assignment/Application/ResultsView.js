c9e704b82bf64e51af85367cedbbedb9a9f812ec
Karthik <kbalasub@stevens.edu>
2020-10-24 23:41:47 -0500

    Added views to render results

--- Assignment/Application/ResultsView.js
+++ Assignment/Application/ResultsView.js
@@ -1,15 +1,38 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import _ from 'lodash';
 
 class ResultsView extends Component {
-    render() {
-        console.log("ResultsView: ", this.props.data)
+
+
+    display(value) {
         return (
-            <View style={{ flex: 1, backgroundColor: '#ADD8E6' }}>
-                <Text>Will Output Results here</Text>
+            <View style={{ flex: 1, margin: 10 }}>
+                <Text>{value.commitHash}</Text>
+                <Text>{value.author}</Text>
+                <Text>{value.commitMessage}</Text>
             </View>
+        )
+    }
+
+
+    render() {
+        let input = this.props && this.props.data && this.props.data.length > 0 ? this.props.data : null
+        console.log("Input: ", input)
+        return (
+            <ScrollView>
+                <View style={{ flex: 1, backgroundColor: '#ADD8E6' }}>
+                    {
+                        input && input.length > 0 ? input.map((value) => {
+                            return <View style={{ borderBottomColor: 'black', borderBottomWidth: 1 }}>
+                                {this.display(value)}
+                            </View>
+                        })
+                            : null
+                    }
+                </View>
+            </ScrollView>
         );
     }
 }
